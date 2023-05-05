2df91efdb95817e92f17182e01775d3ef7b1f39d
Nils Fischer <nils@designatic.de>
2023-05-05 08:21:50 +0200

    fix js to switch question/answer

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,9 @@
-const qcard-question = document.querySelector('[data-js="qcard__question"]');
+const question = document.querySelector('[data-js="qcard__question"]'); //get question
+const answer = document.querySelector('[data-js="qcard__answer"]'); //get answer
+const button = document.querySelector('[data-js="qcard__buttonAnswer"]'); //get button
+
+button.addEventListener("click", () => {
+  //click button
+  question.classList.toggle("qcard__hideMe"); //toggle hideMe question
+  answer.classList.toggle("qcard__hideMe"); //toggle hideMe answer
+});
