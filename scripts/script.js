efe80a7966102b9fa06baaa99596e8226b8c216d
Nils Fischer <nils@designatic.de>
2023-05-05 08:57:11 +0200

    fix bookmarked color

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,19 @@
 const question = document.querySelector('[data-js="qcard__question"]'); //get question
 const answer = document.querySelector('[data-js="qcard__answer"]'); //get answer
+// question
 const button = document.querySelector('[data-js="qcard__buttonAnswer"]'); //get button
 
 button.addEventListener("click", () => {
   //click button
   question.classList.toggle("qcard__hideMe"); //toggle hideMe question
   answer.classList.toggle("qcard__hideMe"); //toggle hideMe answer
+});
+
+// bookmark
+const bookmark = document.querySelector('[data-js="button_bookmark"]'); //get bokmark button
+
+bookmark.addEventListener("click", () => {
+  //click bookmark
+  bookmark.classList.toggle("qcard__bookmarked"); //toggle bookmark-color
+  bookmark.classList.toggle("qcard__bookmark");
 });
