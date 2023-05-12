ce3989418ec8a95634bfe47ff4bc7532ed9012ad
Nils Fischer <nils@designatic.de>
2023-05-12 15:23:12 +0200

    accessing input fields even better

--- components/form/form.js
+++ components/form/form.js
@@ -3,10 +3,13 @@ const form = document.querySelector('[data-js="form-newcard"]');
 form.addEventListener("submit", (event) => {
   event.preventDefault();
 
-  const formQuestion = event.target.elements.formQuestion.value;
-  const formAnswer = event.target.elements.formAnswer.value;
-  const formTag = event.target.elements.formTag.value;
-  console.log(formQuestion);
-  console.log(formAnswer);
-  console.log(formTag);
+  const formData = new FormData(event.target);
+  const data = Object.fromEntries(formData);
+  // const formQuestion = event.target.elements.formQuestion.value;
+  // const formAnswer = event.target.elements.formAnswer.value;
+  // const formTag = event.target.elements.formTag.value;
+
+  console.log(data.formQuestion);
+  console.log(data.formAnswer);
+  console.log(data.formTag);
 });
