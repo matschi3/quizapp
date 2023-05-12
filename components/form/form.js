89d49a632366b8a5e3f9906d2df88c2831f4266f
Nils Fischer <nils@designatic.de>
2023-05-12 14:53:20 +0200

    accessing input fields

--- components/form/form.js
+++ components/form/form.js
@@ -1,1 +1,12 @@
-console.log(true);
+const form = document.querySelector('[data-js="form-newcard"]');
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const formQuestion = event.target.elements.formQuestion.value;
+  const formAnswer = event.target.elements.formAnswer.value;
+  const formTag = event.target.elements.formTag.value;
+  console.log(formQuestion);
+  console.log(formAnswer);
+  console.log(formTag);
+});
