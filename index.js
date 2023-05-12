f98984471489c430a886c74af40c5558981f8a19
Nils Fischer <nils@designatic.de>
2023-05-12 10:38:15 +0200

    edit file structure

---
+++ index.js
@@ -0,0 +1,28 @@
+const question = document.querySelector('[data-js="qcard__question"]'); //get question
+const answer = document.querySelector('[data-js="qcard__answer"]'); //get answer
+// question
+const button = document.querySelector('[data-js="qcard__buttonAnswer"]'); //get button
+
+button.addEventListener("click", () => {
+  //click button
+  question.classList.toggle("qcard__hideMe"); //toggle hideMe question
+  answer.classList.toggle("qcard__hideMe"); //toggle hideMe answer
+});
+
+// bookmark
+const bookmark = document.querySelector('[data-js="button_bookmark"]'); //get bokmark button
+
+bookmark.addEventListener("click", () => {
+  //click bookmark
+  bookmark.classList.toggle("qcard__bookmarked"); //toggle bookmark-color
+  bookmark.classList.toggle("qcard__bookmark");
+});
+
+// new
+// new try - all cards
+/* const qcards = document.querySelectorAll('[data-js="qcard"]');  //get full qcard
+
+qcards.forEach((qcard) => {
+    const answerButton = qcard.querySelector('[data-js=""]');
+    answerButton.classList.
+}); */
