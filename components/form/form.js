c5f53106b79e41fffbb490e058f61b72a0c26acb
Nils Fischer <nils@designatic.de>
2023-05-12 15:57:53 +0200

    generate DOM elements

--- components/form/form.js
+++ components/form/form.js
@@ -9,7 +9,19 @@ form.addEventListener("submit", (event) => {
   // const formAnswer = event.target.elements.formAnswer.value;
   // const formTag = event.target.elements.formTag.value;
 
-  console.log(data.formQuestion);
-  console.log(data.formAnswer);
-  console.log(data.formTag);
+  const newSection = document.createElement("section");
+  const newBookmarkSvg = document.createElement("svg");
+  const newQuestionP = document.createElement("p");
+  const newAnswerP = document.createElement("p");
+  const newAnswerButton = document.createElement("button");
+  const newUlist = document.createElement("ul");
+  const newListitem = document.createElement("li");
+
+  newSection.classList.add("qcard");
+  newBookmarkSvg.classList.add("qcard__bookmark");
+  newQuestionP.classList.add("qcard__question");
+  newAnswerP.classList.add("qcard__answer");
+  newAnswerButton.classList.add("qcard__answerButton");
+  newUlist.classList.add("qcard__taglist");
+  newListitem.classList.add("qcard__taglist__tag");
 });
