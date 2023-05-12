02357ef82bb3b2bc534ece8b5354e44728a7b12a
Nils Fischer <nils@designatic.de>
2023-05-12 16:10:16 +0200

    insert forms data to created elements

--- components/form/form.js
+++ components/form/form.js
@@ -11,17 +11,21 @@ form.addEventListener("submit", (event) => {
 
   const newSection = document.createElement("section");
   const newBookmarkSvg = document.createElement("svg");
-  const newQuestionP = document.createElement("p");
-  const newAnswerP = document.createElement("p");
+  const newQuestion = document.createElement("p");
+  const newAnswer = document.createElement("p");
   const newAnswerButton = document.createElement("button");
   const newUlist = document.createElement("ul");
   const newListitem = document.createElement("li");
 
   newSection.classList.add("qcard");
   newBookmarkSvg.classList.add("qcard__bookmark");
-  newQuestionP.classList.add("qcard__question");
-  newAnswerP.classList.add("qcard__answer");
+  newQuestion.classList.add("qcard__question");
+  newAnswer.classList.add("qcard__answer");
   newAnswerButton.classList.add("qcard__answerButton");
   newUlist.classList.add("qcard__taglist");
   newListitem.classList.add("qcard__taglist__tag");
+
+  newQuestion.textContent = data.formQuestion;
+  newAnswer.textContent = data.formAnswer;
+  newAnswerButton.textContent = `Show Answer`;
 });
