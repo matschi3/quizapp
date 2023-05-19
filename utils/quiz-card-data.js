0b93dd02f53256a789ac450c64678303561f895f
Nils Fischer <nils@designatic.de>
2023-05-19 08:46:27 +0200

    add MODULE and started seperating quizCardsData

---
+++ utils/quiz-card-data.js
@@ -0,0 +1,10 @@
+const quizCardData = [
+  {
+    question: "What property flips the axes in flexbox?",
+    answer: "flex-direction",
+    tag: ["html", "flexbox", "css"],
+    isBookmarked: false,
+  },
+];
+
+export { quizCardData };
