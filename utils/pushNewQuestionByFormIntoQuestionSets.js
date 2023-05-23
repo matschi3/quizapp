73245eb42e78b14d1f6fadeedcd968158b1a32ba
Nils Fischer <nils@designatic.de>
2023-05-23 08:00:03 +0200

    quicksafe

--- utils/pushNewQuestionByFormIntoQuestionSets.js
+++ utils/pushNewQuestionByFormIntoQuestionSets.js
@@ -2,15 +2,15 @@ import { questionSets } from "../../utils/quiz-card-data.js";
 //import { newQuestionByForm } from "../components/form/form.js";
 
 function pushNewQuestionByFormIntoQuestionSets(newFormDataSet) {
-  console.log("inside pushNewQuest:");
+  /*   console.log("inside pushNewQuest:");
   console.log(questionSets);
-  console.log("---/---/---");
+  console.log("---/---/---"); */
 
-  const questionSetsPlus = questionSets.push(newFormDataSet);
-  questionSets = questionSetsPlus;
-  console.log("inside/after pushNewQuest:");
+  /*   const questionSetsPlus =  */ questionSets.push(newFormDataSet);
+  //  questionSets = questionSetsPlus;
+  /*   console.log("inside/after pushNewQuest:");
   console.log(questionSets);
-  console.log("---/---/---");
+  console.log("---/---/---"); */
   return questionSets;
 }
 
